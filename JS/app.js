a2a77ba2ae7a95a904c3f8a597428ce1a9bbf4a0
Alane-Tc <alantamez28@hotmail.com>
2021-01-04 15:00:27 -0600

    delete fuctions

--- JS/app.js
+++ JS/app.js
@@ -5,69 +5,43 @@ window.addEventListener("load", function () {
     var btndog = document.querySelector("#btn-RandomDog")
     var btnCat = document.querySelector("#btn-RandomCats")
     var btnFox = document.querySelector("#btn-RandomFox")
-
     let apiDog = () => { return fetch('https://dog.ceo/api/breeds/image/random') }
-
     let apiCats = () => { return fetch('https://aws.random.cat/meow') }
-
     let apiFox = () => { return fetch('https://randomfox.ca/floof/') }
     function getDogs() {
         apiDog()
             .then(data => data.json())
             .then(imgDog => {
-                randomDog(imgDog)
+                let img = document.querySelector('#img-Dogs')
+                img.src = imgDog.message;
+                img.width = "200"
+                img.height = "200"
+                divDog.appendChild(img);
             })
     }
-
     function getCats() {
         apiCats()
             .then(data => data.json())
-            .then(catsRandom => { randomCat(catsRandom) })
+            .then(catsRandom => {
+                let img = document.querySelector('#img-Cats')
+                img.src = catsRandom.file;
+                img.width = "200"
+                img.height = "200"
+                divCat.appendChild(img);
+            })
     }
-
     function getFox() {
         apiFox()
             .then(data => data.json())
-            .then(foxRandom => {randomFox(foxRandom) })
+            .then(foxRandom => {
+                let img = document.querySelector('#img-Fox')
+                img.src = foxRandom.image;
+                img.width = "200"
+                img.height = "200"
+                divFox.appendChild(img);
+            })
     }
-
-    let randomDog = imgDog => {
-        let img = document.querySelector('#img-Dogs')
-        img.src = imgDog.message;
-        img.width = "200"
-        img.height = "200"
-        divDog.appendChild(img);
-    }
-
-    let randomCat = imgCat => {
-        let img = document.querySelector('#img-Cats')
-        img.src = imgCat.file;
-        img.width = "200"
-        img.height = "200"
-        divCat.appendChild(img);
-    }
-
-    let randomFox = imgFox => {
-        let img = document.querySelector('#img-Fox')
-        img.src = imgFox.image;
-        img.width = "200"
-        img.height = "200"
-        divFox.appendChild(img);
-    }
-
-    btndog.addEventListener("click", function () {
-        getDogs()
-    })
-
-    btnCat.addEventListener("click", function () {
-        getCats()
-    })
-
-    btnFox.addEventListener("click", function () {
-        getFox()
-    })
-
-
-
-
+    btndog.addEventListener("click", function () { getDogs() })
+    btnCat.addEventListener("click", function () { getCats() })
+    btnFox.addEventListener("click", function () { getFox() })
 });
